bb5ea94902be74cf25a7101f68872e43b99e6fae
Guillaume Cugnet <guillaume.cugnet@mary.fr>
2025-02-13 15:12:58 +0100

    feat(V:app): add conditional code which renders text

--- app.vue
+++ app.vue
@@ -1,6 +1,14 @@
+<script setup lang="ts">
+
+const { $userAuth } = useNuxtApp()
+
+</script>
+
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div v-if="$userAuth.role === 'admin'">
+    I am an admin.
+  </div>
+  <div v-else-if="$userAuth.role === 'user'">
+    I am a user.
   </div>
 </template>
